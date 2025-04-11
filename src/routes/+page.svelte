41979257da1b79ebcaab907657eb867e16e7ef41
WillFrancois <williamjcfrancois@gmail.com>
2025-04-10 21:28:02 -0400

    Added loading indicator for auto-mod

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
-	function verifyMessage(): boolean {
+	let loading: boolean = $state(false);
+
+	async function verifyMessage(): Promise<boolean> {
+		loading = true;
 		let userText: HTMLTextAreaElement | null = document.querySelector('#user-input');
 
 		if (userText != null) {
-			console.log(userText.value);
+			let header = new Headers();
+			header.append('Content-Type', 'application/json');
+
+			let response = await fetch('http://localhost:1337/optimessage/verify-message', {
+				method: 'POST',
+				body: JSON.stringify({ message: `${userText.value}` }),
+				headers: header
+			}).then((response) => {
+				loading = false;
+				return response.text();
+			});
+
+			console.log(response);
 		}
 
+		loading = false;
 		return false;
 	}
 </script>
@@ -14,13 +30,20 @@
 
 <form>
 	<div class="grid w-screen place-content-center">
-		<h2 class="mt-5 text-center">Write something nice:</h2>
-		<textarea id="user-input" class="resize-none place-self-center md:w-2/1 lg:w-3/1"></textarea>
+		<h2 class="mt-5 text-center">Write something nice (100 characters max):</h2>
+		<textarea
+			maxlength="100"
+			id="user-input"
+			class="resize-none place-self-center md:w-2/1 lg:w-3/1"
+		></textarea>
 		<button
 			class="mt-1 place-self-center border-1 p-1 md:w-1/2 lg:w-3/4"
 			type="submit"
 			aria-label="Submit Button"
 			onclick={verifyMessage}>Submit</button
 		>
+		<p>
+			{loading}
+		</p>
 	</div>
 </form>
